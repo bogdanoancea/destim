d350cd6d505a9da06ee609960ce391d7f45303d1
Luis Sanguiao <luis.sanguiao.sande@ine.es>
2019-12-02 15:47:02 +0100

    Some corrections with rectangles with min(x,y) < 3

--- R/HMMrectangle.R
+++ R/HMMrectangle.R
@@ -3,6 +3,8 @@
 HMMrectangle <- function(x,y) {
   x <- as.integer(x)
   y <- as.integer(y)
+  if ((x <= 0) || (y <= 0))
+    stop("Both dimensions have to be integer positive numbers.")
   TL <- matrix(0, nrow = 2, ncol = 9*x*y - 6*x - 6*y + 4)
   k <- 1
   for (i in 1:x)
@@ -14,9 +16,29 @@ HMMrectangle <- function(x,y) {
         }
   newmodel <- HMM(x*y, TL)
   TL <- TL[2,] - TL[1,]
-  CT <- matrix(0, nrow = length(TL) - length(unique(TL)) -
-                         (max(x,y) > 2) - (min(x,y) > 2),
+  # Dirty code to deal with x == 2
+  if (x == 2) {
+    if (y == 2)
+      TL[which(apply(transitions(newmodel), 2, function(x)
+         all (x == c(2L,3L)) | all (x == c(3L,2L))))] <- c(5L, -5L)
+    if (y > 2) {
+      TL[which(apply(transitions(newmodel), 2, function(x)
+        (x[2] - x[1] == 1) & (x[1] %% 2 == 0)))] <- 5L
+      TL[which(apply(transitions(newmodel), 2, function(x)
+        (x[2] - x[1] == -1) & (x[1] %% 2 == 1)))] <- -5L
+    }
+  }
+  if (min(x,y) == 1)
+    CT <- matrix(0, nrow = length(TL) - length(unique(TL)) -
+                   (max(x,y) > 2),
+                 ncol = ntransitions(newmodel) + 1)
+  else if (max(x,y) == 2)
+    CT <- matrix(0, nrow = 7,
                ncol = ntransitions(newmodel) + 1)
+  else
+    CT <- matrix(0, nrow = length(TL) - length(unique(TL)) - 1 -
+                             (min(x, y) > 2),
+                 ncol = ntransitions(newmodel) + 1)
   k <- 1
   for (i in unique(TL))
     if (i != 0) {
@@ -30,16 +52,16 @@ HMMrectangle <- function(x,y) {
     }
   stateclasses <- sapply(1:nstates(newmodel), function(n)
                                 sum(transitions(newmodel)[1,] == n))
-  for (j in c(4,6,9)) {
+  for (j in unique(stateclasses)) {
   previous <- 0
   for (i in which(stateclasses == j))
     if (previous == 0)
       previous <- i
     else {
-      CT[k, c((transitions(newmodel)[1, ] == previous) &
-             (TL == 0), FALSE)] <- 1
-      CT[k, c((transitions(newmodel)[1, ] == i) &
-           (TL == 0), FALSE)] <- -1
+      CT[k, c((transitions(newmodel)[1, ] ==
+                 previous) & (TL == 0), FALSE)] <- 1
+      CT[k, c((transitions(newmodel)[1, ] ==
+                 i) & (TL == 0), FALSE)] <- -1
       k <- k + 1
     }
   }
