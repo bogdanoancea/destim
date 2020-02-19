529fc4093b481da8bef3e8fc7400772115cc5952
Luis Sanguiao <luis.sanguiao.sande@ine.es>
2020-02-19 10:03:43 +0100

    Dissimilarity measure added

---
+++ R/dissim.R
@@ -0,0 +1,18 @@
+#' Dissimilarity
+#'
+#' This is an approximate measure
+#'
+#'
+
+dissim <- function(...) {
+  UseMethod("dissim")
+}
+
+#' @rdname dissim
+dissim.HMM <- function(x, y1, y2) {
+  E <- sapply(y2, function(z)
+    if (!is.na(z)) emissions(x)[, z]
+    else rep(1, nstates(x)))
+  output <- apply(E * sstates(x, y1), 2, sum)
+  return(-sum(log(output)) / sum(!is.na(y2)))
+}
